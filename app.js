2985ef391281a505fff84b3cce496215a0c9f9d7
Mohamed Lajmi <mohamedlajmi1995@gmail.com>
2024-07-27 17:26:15 +0100

    v4

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
-const cors = require(cors);
+const cors = require("cors");
 dotenv.config();
 const app = express();
 app.use(
